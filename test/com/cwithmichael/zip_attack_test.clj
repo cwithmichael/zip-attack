3127bccbda8374d06555c44e9a4c6fe99390f606
Michael Terrell <cwithmichael@gmail.com>
2022-05-03 22:06:03 -0500

    adding more unit tests

--- test/com/cwithmichael/zip_attack_test.clj
+++ test/com/cwithmichael/zip_attack_test.clj
@@ -4,9 +4,14 @@
                                                  guess-file-type]]))
 
 (deftest get-header-test
+  (is (nil? (get-header ".")))
+  (is (nil? (get-header nil)))
   (is (= (get-header "jpg") [0xff 0xd8])))
 
 (deftest guess-file-type-test
+  (is (nil? (guess-file-type "")))
+  (is (nil? (guess-file-type nil)))
+  (is (= (guess-file-type ".") ""))
   (is (= (guess-file-type "really.long.file.name.zip") "zip")))
 
 (deftest check-password-test
